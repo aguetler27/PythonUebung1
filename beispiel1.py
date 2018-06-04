9bd415b5e3f51b0d3b460d09f750407cd12e4d94
aguetler28 <aguetler28@icapital.at>
2018-06-04 15:14:31 +0200

    iban code

--- beispiel1.py
+++ beispiel1.py
@@ -1,4 +1,15 @@
 # beispiel1.py
-print ("Hallo Welt!")
-print("Hello Universe!")
-print("Hallo Alle!!!")
+land = "AT"
+blz = "38282"
+konto = "3026713"
+check = "96"
+
+# ziel: formatierte IBAN ausgabe
+# AT96 3828 2000 0302 6713
+p1 = land + check
+p2 = blz[0:4]
+p3 = blz[4:5] + konto[0:3]
+p4 = konto[4:8]
+p5 = konto[9:12]
+print (p1,p2,p3,p4,p5)
+
